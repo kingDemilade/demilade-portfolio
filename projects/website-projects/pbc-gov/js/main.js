2a6666bdbb205403beafe44755872146804429a7
Demilade Haastrup <ddblocka@gmail.com>
2026-04-20 21:02:14 -0400

    Updating JS file

--- projects/website-projects/pbc-gov/js/main.js
+++ projects/website-projects/pbc-gov/js/main.js
@@ -8,17 +8,17 @@ document.addEventListener('DOMContentLoaded', function () {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.style.opacity = 1;
-        entry.target.style.transform = 'translateY(0)';
+        entry.target.style.transform = 'translateY(40)';
       }
     });
   }, {
-    threshold: 0.1
+    threshold: 0.2
   });
 
   cards.forEach(card => {
     card.style.opacity = 0;
     card.style.transform = 'translateY(20px)';
-    card.style.transition = 'all 0.4s ease';
+    card.style.transition = 'all 0.6s ease';
     observer.observe(card);
   });
 });
